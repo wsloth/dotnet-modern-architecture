c975afbd0c010cf554c18e170d6bddb234200941
Wessel Loth <wessel@loth.io>
2019-05-30 23:42:11 +0200

    Add GraphQL controller and empty Query

---
+++ Modern.API.Gateway/Controllers/GraphQLController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Modern.API.Gateway.Controllers
+{
+    public class GraphQLQuery
+    {
+        public string OperationName { get; set; }
+        public string NamedQuery { get; set; }
+        public string Query { get; set; }
+        public JObject Variables { get; set; }
+    }
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GraphQLController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] GraphQLQuery query)
+        {
+            var inputs = query.Variables.ToInputs();
+
+            var schema = new Schema
+            {
+                Query = new AuthorQuery(_db)
+            };
+
+            var result = await new DocumentExecuter().ExecuteAsync(_ =>
+            {
+                _.Schema = schema;
+                _.Query = query.Query;
+                _.OperationName = query.OperationName;
+                _.Inputs = inputs;
+            });
+
+            if (result.Errors?.Count > 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Modern.API.Gateway/GraphQL/Queries/CarQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GraphQL.Types;
+
+namespace Modern.API.Gateway.GraphQL.Queries
+{
+    public class CarQuery : ObjectGraphType
+    {
+        public CarQuery()
+        {
+        }
+    }
+}
